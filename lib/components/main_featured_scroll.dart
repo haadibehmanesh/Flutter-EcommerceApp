79c7112d1cced852f2b551b84b201915f1287a55
unknown <haadibehmanesh@gmail.com>
2019-01-26 15:52:14 +0330

    change main featured

--- lib/components/main_featured_scroll.dart
+++ lib/components/main_featured_scroll.dart
@@ -37,6 +37,7 @@ class _MainFeaturedScrollState extends State<MainFeaturedScroll> {
                             child: Column(
                           children: <Widget>[
                             Container(
+                              //alignment: Alignment.topLeft,
                               constraints: new BoxConstraints.expand(
                                 height: 100.0,
                               ),
@@ -52,31 +53,16 @@ class _MainFeaturedScrollState extends State<MainFeaturedScroll> {
                                     image: new NetworkImage(
                                         'https://boninja.com/storage/bi-products/December2018/rTT72xwlsvsER90Tgv4t.jpg'),
                                     fit: BoxFit.cover),
-                              ), child: new Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          new Icon(
-                            Icons.star,
-                            color: Colors.yellow,
-                          ),
-                          new Center(
-                                child: new Text(
-                                  '50 %',
-                                  style: new TextStyle(
-                                      fontSize: 20.0,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold),
-                                ),
                               ),
-                        ],
-                      ),
+
+                              //child:
                             ),
                             new Row(children: <Widget>[
                               Expanded(
                                 child: Container(
                                   color: Colors.white,
                                   child: new Padding(
-                                    padding: new EdgeInsets.all(7.0),
+                                    padding: new EdgeInsets.symmetric(vertical: 15.0,horizontal: 5.0),
                                     child: new Text(
                                       'فست فود انجیر',
                                       style: new TextStyle(
@@ -87,6 +73,55 @@ class _MainFeaturedScrollState extends State<MainFeaturedScroll> {
                                 ),
                               ),
                             ]),
+                            new Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 0.0, right: 5.0, top: 0.0, bottom: 0.0),
+                              child: new Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  new Column(
+                                    children: <Widget>[
+                                      new Text(
+                                        "50,000  تومان",
+                                        style: new TextStyle(
+                                          fontSize: 10.0,
+                                          color: Colors.red,
+                                          decoration:
+                                              TextDecoration.lineThrough,
+                                        ),
+                                      ),
+                                      new Text(
+                                        "25,000  تومان",
+                                        style: new TextStyle(
+                                            fontSize: 12.0,
+                                            color: Colors.green),
+                                      ),
+                                    ],
+                                  ),
+                                  new Row(
+                                    children: <Widget>[],
+                                  ),
+                                  new Container(
+                                      width: 40.0,
+                                      height: 40.0,
+                                      decoration: new BoxDecoration(
+                                        //gradient: btnGradient,
+                                        color: Colors.orange,
+                                        shape: BoxShape.circle,
+                                      ),
+                                      child: Center(
+                                        child: new Text(
+                                          '50 %',
+                                          style: new TextStyle(
+                                              fontSize: 14.0,
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                      )),
+                                ],
+                              ),
+                            ),
                           ],
                         )),
                       )),
