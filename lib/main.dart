58617f71f120261e51aab93da778433df7184c42
unknown <haadibehmanesh@gmail.com>
2019-01-27 15:39:56 +0330

    change flutter launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import './pages/home.dart';
 
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
